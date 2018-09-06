b12a9a90f733762b532c6e6f9d7a7e07bc20213d
Susumu Yamazaki <zacky1972@gmail.com>
2018-09-06 10:30:03 +0900

    implement the function of reading tweet_index.js

--- lib/tw_diary.ex
+++ lib/tw_diary.ex
@@ -3,4 +3,10 @@ defmodule TwDiary do
   Documentation for TwDiary.
   """
 
+  index_file = File.read!("data/data/js/tweet_index.js")
+  map_list = index_file
+  |> String.split("= ")
+  |> List.last
+  |> Poison.decode!
+  IO.inspect map_list
 end
