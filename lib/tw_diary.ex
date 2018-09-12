b51a7903c7ac4ea9b61830a9659e843b4070ca99
zacky1972 <zacky1972@gmail.com>
2018-09-12 14:45:02 +0900

    add sorting function

--- lib/tw_diary.ex
+++ lib/tw_diary.ex
@@ -16,7 +16,13 @@ defmodule TwDiary do
     |> Enum.map(& "data/#{&1}")
     |> Enum.map(& json_read(&1))
     |> Enum.flat_map(& &1)
-    |> Enum.map(& %{ :text => &1["text"], :date => &1["created_at"]})
+    |> Enum.map(& %{ :text => &1["text"], :date => get_datetime(&1["created_at"])})
+    |> Enum.reverse
+    |> Enum.sort(& (Timex.compare(&1[:date] , &2[:date]) <= 0))
     |> IO.inspect
+  end
+
+  def get_datetime(string) do
+    Timex.parse!(string, "%F %T %z", :strftime)
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,14 +14,16 @@ defmodule TwDiary.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      applications: [:timex]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:timex, "~> 3.1"}
     ]
   end
 end
