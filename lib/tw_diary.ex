eeb8109b0aa7d0471c3ff3765a5da1f44a032df2
Susumu Yamazaki <zacky1972@gmail.com>
2018-09-12 09:14:10 +0900

    create json_read and all_read

--- lib/tw_diary.ex
+++ lib/tw_diary.ex
@@ -3,10 +3,18 @@ defmodule TwDiary do
   Documentation for TwDiary.
   """
 
-  index_file = File.read!("data/data/js/tweet_index.js")
-  map_list = index_file
-  |> String.split("= ")
-  |> List.last
-  |> Poison.decode!
-  IO.inspect map_list
+  def json_read(file) do
+    File.read!(file)
+    |> String.split("= ")
+    |> List.last
+    |> Poison.decode!
+  end
+
+  def all_read() do
+    json_read("data/data/js/tweet_index.js")
+    |> Enum.map(& &1["file_name"])
+    |> Enum.map(& "data/#{&1}")
+    |> Enum.map(& json_read(&1))
+    |> Enum.map(& IO.inspect &1)
+  end
 end
